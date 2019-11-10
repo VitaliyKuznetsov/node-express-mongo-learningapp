5a0d2e2a2448b8515850d91982b6b150ef7b2554
Vitaliy Kuznetsov <work@kuznetsovvp.ru>
2019-11-10 18:20:16 +0300

    Express Example
    Prev commit was wrong

--- node-express/index.js
+++ node-express/index.js
@@ -7,16 +7,12 @@ const hostname = 'localhost',
 
 const app = express();
 
-// app.use((req,res,next) => {
-//     console.log(req.headers);
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-// });
-
-app.use(morgan('dev'));
-
-app.use(express.static(__dirname + '/public'));
+app.use((req,res,next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
 
 const server = http.createServer(app);
 
