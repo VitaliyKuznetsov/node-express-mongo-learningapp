44a1000d98d0cb2fd9528cf11a9d32043e01f09c
Vitaliy Kuznetsov <work@kuznetsovvp.ru>
2019-11-10 18:20:31 +0300

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -7,12 +7,16 @@ const hostname = 'localhost',
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-});
+// app.use((req,res,next) => {
+//     console.log(req.headers);
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+// });
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 
