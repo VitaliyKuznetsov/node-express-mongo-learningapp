f9989a473c31e41c3e9696a2c12f3595b53739ad
Vitaliy Kuznetsov <work@kuznetsovvp.ru>
2019-11-10 17:19:06 +0300

    Node Callbacks and Error Handling Configuring vscode run task

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,18 +1,21 @@
-// var rect = {
-//     perimeter: (x,y) => (2*(x+y)),
-//     area: (x,y) => (x*y)
-// };
 var rect = require("./rectangle");
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = "+l+" and b = "+b);
     
-    if(l<=0||b<=0) console.log("Rectangle dims should be greater then zero: \n\tl is: "+l+"\n\tb is: "+b);
-    else{
-        console.log("Area of rectangle is: "+rect.area(l,b));
-        console.log("Perimeter of rectangle is: "+rect.perimeter(l,b));
-    }
-}
+    rect(l,b,(err, rectangle) => {
+        if(err){
+            console.log("ERROR: "+err);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());          
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
 solveRect(2,4);
 solveRect(3,5);
