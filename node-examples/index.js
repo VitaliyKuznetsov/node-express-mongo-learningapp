bf4020e6fd4a646e8cd14fb1bc46e787d53766ef
Vitaliy Kuznetsov <work@kuznetsovvp.ru>
2019-11-10 16:33:15 +0300

    Simple Node Example

---
+++ node-examples/index.js
@@ -0,0 +1,19 @@
+var rect = {
+    primeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "+l+" and b = "+b);
+    
+    if(l<=0||b<=0) console.log("Rectangle dims should be greater then zero: \n\tl is: "+l+"\n\tb is: "+b);
+    else{
+        console.log("Area of rectangle is: "+rect.area(l,b));
+        console.log("Perimeter of rectangle is: "+rect.primeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
